a0d19b266fbe2016c8a034c81e3820c7ff0b7518
deda9 <Bassem.Qoulta@gmail.com>
2020-05-20 10:58:31 +0200

    Create home page

---
+++ MovizySwiftUI/MovizySwiftUI/Discover/DiscoverHomeView.swift
@@ -0,0 +1,15 @@
+//
+//  DiscoverHomeView.swift
+//  MovizySwiftUI
+//
+//  Created by Deda on 20.05.20.
+//  Copyright © 2020 Deda. All rights reserved.
+//
+
+import SwiftUI
+
+struct DiscoverHomeView: View {
+    var body: some View {
+        Text("DiscoverHomeView")
+    }
+}
---
+++ MovizySwiftUI/MovizySwiftUI/Movies/MoviesHomeView.swift
@@ -0,0 +1,15 @@
+//
+//  MoviesHomeView.swift
+//  MovizySwiftUI
+//
+//  Created by Deda on 20.05.20.
+//  Copyright © 2020 Deda. All rights reserved.
+//
+
+import SwiftUI
+
+struct MoviesHomeView: View {
+    var body: some View {
+        Text("MoviesHomeView")
+    }
+}
