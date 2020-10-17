44ae6be4f58273fbfafc75107ac228c09133e192
deda9 <Bassem.Qoulta@gmail.com>
2020-10-17 22:08:17 +0200

    Add common file into common module

---
+++ Common/Common/Constants.swift
@@ -0,0 +1,16 @@
+public enum Path {
+    public enum Movies {
+        public static let popularList = "/movie/popular"
+        public static let searchMovie = "/search/movie"
+        public static let imageBaseUrl = "https://image.tmdb.org/t/p/w500"
+    }
+}
+
+public enum Keys {
+    public static let MovieDB = "9075303a6c0299a1dc0e2d6eaa1cffed"
+}
+
+public enum Language {
+    public static let en = "en-US"
+}
+
