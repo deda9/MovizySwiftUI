dea198e93d357d3b3e501879cb67a4523cd6568f
deda9 <Bassem.Qoulta@gmail.com>
2020-10-18 16:16:58 +0200

    Remove - unused code

--- MovizySwiftUI/Movies/Movies/MoviesViewModel.swift
+++ MovizySwiftUI/Movies/Movies/MoviesViewModel.swift
@@ -44,70 +44,14 @@ public final class MoviesViewModel: MoviesViewModelProtocol, MoviesViewModelInpu
                 guard let self = self else { return }
                 switch result {
                 case .success(let moviesResponse):
-                    delay(seconds: 3, thread: .main) {
-                        
-                    
                     self.isLoading = false
                     self.movies = moviesResponse.getMovies()
                     
-                    }
                 case .failure(let error):
                     self.isLoading = false
                     self.errorMessage = error.errorDescription
                 }
             }
         request.store(in: &bag)
-    }
-}
-
-
-enum DelayThread {
-    case background(DispatchQoS.QoSClass)
-    case main
-}
-
-func delay(seconds: Double, thread: DelayThread, execute: @escaping @convention(block) () -> Void) {
-    switch thread {
-    case .main:
-        DispatchQueue.main.asyncAfter(seconds: seconds, execute: execute)
-    case .background(let qos):
-        DispatchQueue.global(qos: qos).asyncAfter(seconds: seconds, execute: execute)
-    }
-}
-
-func delay(seconds: Int, thread: DelayThread, execute: @escaping @convention(block) () -> Void) {
-    switch thread {
-    case .main:
-        DispatchQueue.main.asyncAfter(seconds: seconds, execute: execute)
-    case .background(let qos):
-        DispatchQueue.global(qos: qos).asyncAfter(seconds: seconds, execute: execute)
-    }
-}
-
-extension DispatchQueue {
-    func asyncAfter(seconds: Int, qos: DispatchQoS = .default, execute work: @escaping @convention(block) () -> Void) {
-        self.asyncAfter(deadline: DispatchTime(seconds), qos: qos, execute: work)
-    }
-    
-    func asyncAfter(seconds: Int, execute work: @escaping @convention(block) () -> Void) {
-        self.asyncAfter(deadline: DispatchTime(seconds), execute: work)
-    }
-    
-    func asyncAfter(seconds: Double, qos: DispatchQoS = .default, execute work: @escaping @convention(block) () -> Void) {
-        self.asyncAfter(deadline: DispatchTime(seconds), qos: qos, execute: work)
-    }
-    
-    func asyncAfter(seconds: Double, execute work: @escaping @convention(block) () -> Void) {
-        self.asyncAfter(deadline: DispatchTime(seconds), execute: work)
-    }
-}
-
-extension DispatchTime {
-    public init(_ value: Int) {
-        self = .now() + .seconds(value)
-    }
-    
-    public init(_ value: Double) {
-        self = .now() + .milliseconds(Int(value * 1000))
     }
 }
